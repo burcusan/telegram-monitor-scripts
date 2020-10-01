78ce04fff66ed2107d5899ab12f5587880b14dfd
burcusan <66318072+burcusan@users.noreply.github.com>
2020-10-01 20:49:33 +0300

    Create install_node_exporter.sh

---
+++ install_node_exporter.sh
@@ -0,0 +1,23 @@
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
+sudo useradd -rs /bin/false node_exporter
+sudo vi /etc/systemd/system/node_exporter.service
+
+sudo cat <<- EOF > /etc/systemd/system/node_exporter.service    
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter
+sudo systemctl enable node_exporter
